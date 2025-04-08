b864edd3e0022d18345450c9109d8b7e721ef36f
Rodrigo Merlo <rodrigom@Excoeng.com>
2025-04-08 09:54:14 -0400

    Finished sample app

---
+++ EpicorRestApiHelloWorld/EpicorApiFunctions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+
+class EpicorApiFunctions
+{
+    // THESE FUNCTIONS ARE "GENERIC" TO INTERFACE WITH THE EPICOR REST API
+    // DOCS: https://ee-erp11-test.excoeng.com/ERP11-TST/api/help/v2/index.html
+    // IN OTHER PROJECTS THE EPICOR OBJECTS RELY ON THESE TO GET/PUSH/PATCH/DELETE DATA FROM EPICOR
+    // IT IS UP TO YOU IF YOU WANT TO PUT THIS LOGIC ON YOUR OBJECTS OR KEEP SEPARATE, THIS IS JUST AN EXAMPLE.
+
+    private static HttpClient GetAuthenticatedClientFromEnviromentVariables(string uri_address)
+    {
+        string userName = Environment.GetEnvironmentVariable("EPICOR_EXCO_API_READONLY_SERVER_USER");
+        string password = Environment.GetEnvironmentVariable("EPICOR_EXCO_API_READONLY_SERVER_PASSWORD");
+        string currentApiKey = Environment.GetEnvironmentVariable("EPICOR_EXCO_API_KEY");
+
+        if (string.IsNullOrWhiteSpace(userName) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(currentApiKey))
+        {
+            string message = "One or more required environment variables are missing:\n" +
+                             $"{(string.IsNullOrWhiteSpace(userName) ? "- EPICOR_EXCO_API_READONLY_SERVER_USER\n" : "")}" +
+                             $"{(string.IsNullOrWhiteSpace(password) ? "- EPICOR_EXCO_API_READONLY_SERVER_PASSWORD\n" : "")}" +
+                             $"{(string.IsNullOrWhiteSpace(currentApiKey) ? "- EPICOR_EXCO_API_KEY\n" : "")}";
+            throw new InvalidOperationException("Required Epicor API environment variables are missing.");
+        }
+
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(uri_address)
+        };
+
+        string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{userName}:{password}"));
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+        httpClient.DefaultRequestHeaders.Add("x-api-key", currentApiKey);
+        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        httpClient.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue { NoCache = true };
+
+        return httpClient;
+    }
+
+    private static string GetCurrentServer(bool useTestServer = true)
+    {
+        string CURRENT_COMPANY = "200";
+        string PRD_SERVER = $"https://ee-erp11-app.excoeng.com/ERP11-PRD/api/v2/odata/{CURRENT_COMPANY}/";
+        string TEST_SERVER = $"https://ee-erp11-test.excoeng.com/ERP11-TST/api/v2/odata/{CURRENT_COMPANY}/";
+
+        string currentServer = null;
+        if (useTestServer)
+        {
+            currentServer = TEST_SERVER;
+        }
+        else if (!useTestServer)
+        {
+            currentServer = PRD_SERVER;
+        }
+        return currentServer;
+    }
+
+    public static string HitEpicorWithAGet(string epicorService, string parameterString, string argumentString = "", bool useTestServer = true)
+    {
+        string currentServer = GetCurrentServer(useTestServer);
+        string baseUri = $"";
+        if (argumentString != "")
+        {
+            baseUri = $"{currentServer}{epicorService}({argumentString})?{parameterString}";
+        }
+        else
+        {
+            baseUri = $"{currentServer}{epicorService}?{parameterString}";
+        }
+        UriBuilder uriBuilder = new UriBuilder(baseUri);
+        HttpClient httpClient = GetAuthenticatedClientFromEnviromentVariables(uriBuilder.ToString());
+        string responseBody = "";
+        try
+        {
+            if (useTestServer) // test server usually no ssl
+            {
+                ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            }
+
+            HttpResponseMessage response = httpClient.GetAsync("").GetAwaiter().GetResult(); // GET no arguments no parameters no body
+            responseBody = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            response.EnsureSuccessStatusCode();
+            return responseBody;
+        }
+        catch //(HttpRequestException e)
+        {
+            string issue = $"There was an issue during GET to service {epicorService}";
+            Debug.Write($"ISSUE, {issue}");
+            Debug.Write($"URI, {baseUri}");
+            Debug.Write($"RESPONSEBODY, {responseBody}");
+            Debug.Write($"\n");
+            return null;
+        }
+    }
+}
---
+++ EpicorRestApiHelloWorld/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// DOCS: https://ee-erp11-test.excoeng.com/ERP11-TST/api/help/v2/index.html
+namespace EpicorRestApiHelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+            DataTable testTableFromNormalService = LoadDataFromNormalEpicorService();
+            DataTable testTableFromCustomService = LoadDataFromCustomEpicorService();
+            dataGridView1.DataSource = testTableFromNormalService;
+            dataGridView2.DataSource = testTableFromCustomService;
+        }
+
+        private DataTable LoadDataFromNormalEpicorService()
+        {
+            string epicorService = "Erp.Bo.QuoteSvc/Quotes";
+            int top = 20;
+            string orderby = "QuoteNum";
+            string fields = "QuoteNum, CustomerCustID";
+            string parameterString = $"$select={fields}&$orderby={orderby} desc&$top={top}";
+
+            string responseBody = EpicorApiFunctions.HitEpicorWithAGet(epicorService, parameterString);
+
+            if (responseBody == null)
+            {
+                MessageBox.Show("No data received from Epicor.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+
+            // var responseObject = JsonConvert.DeserializeObject<QuoteResponse>(responseBody);
+            // Comment from Rodrigo: At this point you can deserialize the json into one of your objects if you're feeling fancy
+            // On this example I just use the json to return a DataTable for clarity
+
+            // Parse JSON into a JObject
+            JObject obj = JObject.Parse(responseBody);
+
+            // Epicor returns data inside the `value` property
+            JArray rows = (JArray)obj["value"];
+
+            // Convert JArray to DataTable
+            DataTable table = JsonConvert.DeserializeObject<DataTable>(rows.ToString());
+            return table;
+        }
+
+        private DataTable LoadDataFromCustomEpicorService()
+        {
+            // This requires a BAQ to be setup in Epicor for the query, helps with really complicated joins/filters etc, should also be a lot faster than fetching everything and messing with it in code.
+            // Rodrigo, Shea or Adam can help setting this up in Epicor so your app just gets the table it needs, ask for a "BAQ" (Business Activity Query)
+            
+            string epicorService = "BaqSvc/RM-JobAsmblTest/Data"; // You can also do /$metadata if you want to be really fancy and fetch the table structure dynamically. 
+            int top = 20;
+            string orderby = "JobOper_StartDate";
+            string fields = "JobOper_StartDate, JobAsmbl_JobNum, JobAsmbl_AssemblySeq, JobAsmbl_PartNum, JobOper_JobComplete, JobOper_OpComplete";
+            string parameterString = $"$select={fields}&$orderby={orderby} desc&$top={top}";
+
+            string responseBody = EpicorApiFunctions.HitEpicorWithAGet(epicorService, parameterString, useTestServer: false); // by passing false there you hit the prod server instead of the test Epicor server.
+
+            if (responseBody == null)
+            {
+                MessageBox.Show("No data received from Epicor.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+
+            // Parse JSON into a JObject
+            JObject obj = JObject.Parse(responseBody);
+
+            // Epicor returns data inside the `value` property
+            JArray rows = (JArray)obj["value"];
+
+            // Convert JArray to DataTable
+            DataTable table = JsonConvert.DeserializeObject<DataTable>(rows.ToString());
+            return table;
+        }
+
+
+
+
+
+        // OTHER STUFF
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
